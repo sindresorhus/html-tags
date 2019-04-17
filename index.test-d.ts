9541b586eeda13f2f2068a1eba58e5710731cf2e
Dimitri Benin <BendingBender@users.noreply.github.com>
2019-04-17 07:09:10 +0000

    Require Node.js 8, add TypeScript definition (#3)

---
+++ index.test-d.ts
@@ -0,0 +1,14 @@
+import {expectType, expectError} from 'tsd';
+import htmlTags = require('.');
+import voidHtmlTags = require('./void');
+import htmlTagsJson = require('./html-tags.json');
+import voidHtmlTagsJson = require('./html-tags-void.json');
+
+expectType<readonly string[]>(htmlTags);
+expectError(htmlTags.push(""));
+expectType<readonly string[]>(voidHtmlTags);
+expectError(voidHtmlTags.push(""));
+expectType<readonly string[]>(htmlTagsJson);
+expectError(htmlTagsJson.push(""));
+expectType<readonly string[]>(voidHtmlTagsJson);
+expectError(voidHtmlTagsJson.push(""));
