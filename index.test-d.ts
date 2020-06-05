5eb6dbca864b7333fa551cfb9772ad8b2c8ddd17
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2020-06-05 08:17:47 +0300

    Make the TypeScript type a union of the HTML tags (#5)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,9 +1,10 @@
 import {expectType, expectError} from 'tsd';
-import htmlTags = require('.');
+import htmlTags, {HTMLTags} from '.'
 import voidHtmlTags = require('./void');
 import htmlTagsJson = require('./html-tags.json');
 import voidHtmlTagsJson = require('./html-tags-void.json');
 
+expectType<readonly HTMLTags[]>(htmlTags);
 expectType<readonly string[]>(htmlTags);
 expectError(htmlTags.push(''));
 expectType<readonly string[]>(voidHtmlTags);
