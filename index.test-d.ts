58bbdfceecd205b0f7c62975f86cce01272a3a0b
Sergey Sova <5620073+sergeysova@users.noreply.github.com>
2020-06-05 08:29:54 +0300

    fix tests

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,15 +1,15 @@
 import {expectType, expectError} from 'tsd';
 import htmlTags, {HTMLTags} from '.'
-import voidHtmlTags = require('./void');
+import htmlTagsVoid, {HTMLTagsVoid} from './void'
 import htmlTagsJson = require('./html-tags.json');
-import voidHtmlTagsJson = require('./html-tags-void.json');
+import htmlTagsVoidJson = require('./html-tags-void.json');
 
 expectType<readonly HTMLTags[]>(htmlTags);
 expectType<readonly string[]>(htmlTags);
 expectError(htmlTags.push(''));
-expectType<readonly string[]>(voidHtmlTags);
-expectError(voidHtmlTags.push(''));
+expectType<readonly HTMLTagsVoid[]>(htmlTagsVoid);
+expectError(htmlTagsVoid.push(''));
 expectType<readonly string[]>(htmlTagsJson);
 expectError(htmlTagsJson.push(''));
-expectType<readonly string[]>(voidHtmlTagsJson);
-expectError(voidHtmlTagsJson.push(''));
+expectType<readonly HTMLTagsVoid[]>(htmlTagsVoidJson);
+expectError(htmlTagsVoidJson.push(''));
