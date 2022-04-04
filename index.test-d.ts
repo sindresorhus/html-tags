0f393869e647b54d11830c04fc812e2ba3f499c6
Binyamin Aron Green <binyamingreen@protonmail.com>
2022-04-04 11:00:21 -0400

    ensure cjs-comptible, and remove json typedefs

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,15 +1,11 @@
 import {expectType, expectError} from 'tsd';
-import htmlTags, {HTMLTags} from '.'
-import htmlTagsVoid, {HTMLTagsVoid} from './void'
-import htmlTagsJson = require('./html-tags.json');
-import htmlTagsVoidJson = require('./html-tags-void.json');
+import htmlTags from '.'
+import htmlTagsVoid from './void'
 
-expectType<readonly HTMLTags[]>(htmlTags);
+expectType<readonly htmlTags.htmlTags[]>(htmlTags);
 expectType<readonly string[]>(htmlTags);
 expectError(htmlTags.push(''));
-expectType<readonly HTMLTagsVoid[]>(htmlTagsVoid);
+
+expectType<readonly htmlTagsVoid.htmlTagsVoid[]>(htmlTagsVoid);
+expectType<readonly string[]>(htmlTags);
 expectError(htmlTagsVoid.push(''));
-expectType<readonly string[]>(htmlTagsJson);
-expectError(htmlTagsJson.push(''));
-expectType<readonly HTMLTagsVoid[]>(htmlTagsVoidJson);
-expectError(htmlTagsVoidJson.push(''));
