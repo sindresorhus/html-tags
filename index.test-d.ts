3da333812411bceeab796ec60929dd423d7113be
Binyamin Aron Green <binyaminagreen@gmail.com>
2022-04-06 03:30:26 -0400

    Add TypeScript types as union type (#8)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,15 +1,11 @@
 import {expectType, expectError} from 'tsd';
-import htmlTags, {HTMLTags} from '.'
-import voidHtmlTags = require('./void');
-import htmlTagsJson = require('./html-tags.json');
-import voidHtmlTagsJson = require('./html-tags-void.json');
+import htmlTags = require('.');
+import htmlTagsVoid = require('./void');
 
-expectType<readonly HTMLTags[]>(htmlTags);
+expectType<readonly htmlTags.htmlTags[]>(htmlTags);
 expectType<readonly string[]>(htmlTags);
 expectError(htmlTags.push(''));
-expectType<readonly string[]>(voidHtmlTags);
-expectError(voidHtmlTags.push(''));
-expectType<readonly string[]>(htmlTagsJson);
-expectError(htmlTagsJson.push(''));
-expectType<readonly string[]>(voidHtmlTagsJson);
-expectError(voidHtmlTagsJson.push(''));
+
+expectType<readonly htmlTagsVoid.htmlTagsVoid[]>(htmlTagsVoid);
+expectType<readonly string[]>(htmlTags);
+expectError(htmlTagsVoid.push(''));
